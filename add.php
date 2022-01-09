3a5f92872985ad0c32f96768e174ff7a9a742e9b
Tran Thuat <thuattv72@wru.vn>
2022-01-09 13:15:38 +0700

    first commit

--- add.php
+++ add.php
@@ -1,29 +1,3 @@
-<?php
-    // deleteEmployee: NHẬN DỮ LIỆU TỪ admin.php gửi sang
-    $madg = $_GET['id'];
-
-    // Bước 01: Kết nối Database Server
-    $conn = mysqli_connect('localhost','root','','175a071520_libraries');
-    if(!$conn){
-        die("Kết nối thất bại. Vui lòng kiểm tra lại các thông tin máy chủ");
-    }
-    // Bước 02: Thực hiện truy vấn
-    $sql = "SELECT * FROM db_docgia WHERE madg = '$madg";
-
-    $result = mysqli_query($conn,$sql); //Nó chỉ ra về 1 bản ghi, mà mình chỉ cần lấy 1 bản ghi thôi
-
-    // Bước 03: Xử lý kết quả
-    if(mysqli_num_rows($result) > 0){
-        $row = mysqli_fetch_assoc($result);
-        $ma_donvi = $row['madg'];
-    }
-
-    // Bước 04: Đóng kết nối
-    mysqli_close($conn);
-
-
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -39,64 +13,85 @@
         <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">TLU - Administration</a>
+                <a class="navbar-brand" href="#">Thêm - Admin</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
-</div>
+                
+            </div>
             </nav>
         </div>
     </header>
     <main>
     <div class="container">
-        <h5 class="text-center text-primary mt-5">Cập nhật thông tin Danh bạ nhân viên</h5>
+        <h5 class="text-center text-primary mt-5">Thêm mới Danh bạ</h5>
         <!-- Form thêm Dữ liệu nhân viên -->
-        <form action="process-update-employee.php" method="post">
-            <div class="form-group">
-                <label for="txtdocgia">Mã Độc Giả</label>
-                <input type="text" class="form-control" id="txtdocgia" name="txtdocgia" placeholder="Mã đọc giả" value="<?php echo $row['madg']; ?>">
-                <!-- <small id="txtHoTenHelp" class="form-text text-muted">Có thể dùng nó hiển thị thông báo lỗi hoặc gợi ý</small> -->
-            </div>
-
-            <div class="form-group">
+        <form action="process-add-employee.php" method="post">
+        <div class="form-group">
                 <label for="txtHoTen">Họ và tên</label>
                 <input type="text" class="form-control" id="txtHoTen" name="txtHoTen" placeholder="Nhập họ tên" value="<?php echo $row['hovaten']; ?>">
                 <!-- <small id="txtHoTenHelp" class="form-text text-muted">Có thể dùng nó hiển thị thông báo lỗi hoặc gợi ý</small> -->
             </div>
             
             <div class="form-group">
-                <label for="txtChucVu">Giới Tính</label>
+                <label for="txtChucVu">giới tính</label>
                 <input type="text" class="form-control" id="txtgioitinh" name="txtgioitinh" placeholder="Nhập gioi tinh" value="<?php echo $row['gioitinh']; ?>">
                 <!-- <small id="txtHoTenHelp" class="form-text text-muted">Có thể dùng nó hiển thị thông báo lỗi hoặc gợi ý</small> -->
             </div>
 
             <div class="form-group">
-                <label for="txtnamsinh">Năm Sinh</label>
+                <label for="txtnamsinh">năm sinh</label>
                 <input type="text" class="form-control" id="txtnamsinh" name="txtnamsinh" placeholder="Nhập nam sinh" value="<?php echo $row['namsinh']; ?>">
                 <!-- <small id="txtHoTenHelp" class="form-text text-muted">Có thể dùng nó hiển thị thông báo lỗi hoặc gợi ý</small> -->
             </div>
             <div class="form-group">
-                <label for="txtnghenghiep">Nghề Nghiệp</label>
+                <label for="txtnghenghiep">Nghe Nghiep</label>
                 <input type="text" class="form-control" id=txtnghenghiep" name="txtnghenghiep" placeholder="nghenghiep" value="<?php echo $row['nghenghiep']; ?>">
                 
             </div>
             <div class="form-group">
-                <label for="txtngaycapthe">Ngày Cấp Thẻ</label>
+                <label for="txtngaycapthe">ngay cap the</label>
                 <input type="date" class="form-control" id="txtngaycapthe" name="txtngaycapthe" placeholder="Nhập ngay cap the" value="<?php echo $row['ngaycapthe']; ?>">
                
             </div>
             <div class="form-group">
-                <label for="txtngayhethan">Ngày Hết Hạn</label>
+                <label for="txtngayhethan">ngay hethan</label>
                 <input type="date" class="form-control" id="txtngayhethan" name="txtngayhethan" placeholder="Nhập ngay hethan" value="<?php echo $row['ngayhethan']; ?>">
                
             </div>
             <div class="form-group">
-                <label for="txtdiachi">Địa Chỉ</label>
+                <label for="txtdiachi">dia chi</label>
                 <input type="text" class="form-control" id="txtdiachi" name="txtdiachi" placeholder="Nhập dia chi" value="<?php echo $row['diachi']; ?>">
                
             </div>
-            
-            <button type="submit" class="btn btn-primary mt-3">Lưu Lại</button>
+                    <!-- Truy vấn dữ liệu để Hiển thị lựa chọn Đơn vị -->
+                    <?php 
+                        // Bước 01: Kết nối Database Server
+                        $conn = mysqli_connect('localhost','root','','dhtl_danhba');
+                        if(!$conn){
+                            die("Kết nối thất bại. Vui lòng kiểm tra lại các thông tin máy chủ");
+                        }
+                        // Bước 02: Thực hiện truy vấn
+                        $sql = "SELECT * FROM db_donvi";
+
+                        $result = mysqli_query($conn,$sql);
+
+                        // Bước 03: Xử lý kết quả truy vấn
+                        if(mysqli_num_rows($result)){
+                            while($row = mysqli_fetch_assoc($result)){
+                    ?>
+                                <option value="<?php echo $row['ma_donvi']; ?>"><?php echo $row['ten_donvi']; ?></option>
+                    <?php
+                            }
+                        }
+
+                        // Bước 03: Đóng kết nối
+                        mysqli_close($conn);
+                    ?>
+               
+                </select>
+            </div>
+            <button type="submit" class="btn btn-primary mt-3">Lưu lại</button>
         </form>
     </div>    
     </main>
